91cbaaf45cbd4bbb6ac539402bb5e91853f3fbce
David Sapp <>
2021-03-24 17:45:00 -0600

    switch GA to Gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,10 +15,19 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: config.siteGATrackingID
-      }
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          config.siteGATrackingID, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      },
     },
     `gatsby-plugin-notifications`,
     `gatsby-plugin-sharp`,
